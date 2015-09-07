b2b3a4969a37bb1e44d8cd54aa93b77574e76077
Zhiyuan <zhiyuan.richard.cui@gmail.com>
2015-09-06 21:30:54 -0400

    fix buges

--- src/Test/Test.java
+++ src/Test/Test.java
@@ -2,57 +2,64 @@ package Test;
 
 import java.util.concurrent.ArrayBlockingQueue;
 
+
+
+
 public class Test {
+	
+	private BlockingQueue<Integer> q = new BlockingQueue<Integer>(3);
+	
+	class Producer extends Thread
+	{
+		int count = 0;
+		public Producer(int c)
+		{
+			count = c;
+		}
+		
+		@Override
+	    public void run() {
+	        for (int i = 0; i < 4; i++) {
+	            try {
+	                Thread.sleep(1000);
+	                q.offer(count++);
+	                System.out.println("Producer1 added: ");
+	            } catch (InterruptedException e) {
+	                e.printStackTrace();
+	            }
+	        }
+	    }
+	}
+
+		
+	class Consumer extends Thread
+	{
+		@Override
+	    public void run() {
+	        for (int i = 0; i < 8; i++) {
+	            try {
+	                Thread.sleep(3500);
+	                System.out.println("Consumer removed: "+q.poll());
+	            } catch (InterruptedException e) {
+	                e.printStackTrace();
+	            }
+	            
+	        }
+		}
+	}
+	
+	
+	
 	
 	public static void main(String[] args)
 	{
-		BlockingQueue<Integer> q = new BlockingQueue<Integer>(3);
-		new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 0; i < 8; i++) {
-                    try {
-                        Thread.sleep(3500);
-                        System.out.println("Consumer removed: "+q.poll());
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    
-                }
-            }
-        }).start();
-
-		//Thread for producer1
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 0; i < 4; i++) {
-                    try {
-                        Thread.sleep(1000);
-                        q.offer(i);
-                        System.out.println("Producer1 added: ");
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }).start();
-		        
-      //Thread for producer2        
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 0; i < 4; i++) {
-                    try {
-                        Thread.sleep(1000);
-                        q.offer(i+10);
-                        System.out.println("Producer2 added: ");
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }).start();
+		Test t = new Test();
+		Producer p1 = t.new Producer(1);
+		Producer p2 = t.new Producer(10);
+		Consumer c1 = t.new Consumer();
 		
+		p1.start();
+		p2.start();
+		c1.start();
 	}
 }
